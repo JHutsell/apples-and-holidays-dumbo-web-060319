079fcc29c863a46e66d4332256bed45f034d7ac3
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 20:39:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |seasons, info|
-    season = 
+  holiday_hash.each do |seasons, holidays|
+    
     
   end
 end
